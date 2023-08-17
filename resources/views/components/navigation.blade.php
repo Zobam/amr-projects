494ec3473b8f057b0f785a7fc56335cc361d46cc
Donzoby <upc4you@gmail.com>
2023-08-17 13:58:02 +0100

    feat: added active link styles to the nav bar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/contact">Contact Us</a></li>
+        <li><a href="/" class="{{ (request()->is('/')) ? 'active' : '' }}">Home</a></li>
+        <li><a href="/contact" class="{{ (request()->is('contact')) ? 'active activeContact' : '' }}">Contact Us</a></li>
     </ul>
 </nav>
