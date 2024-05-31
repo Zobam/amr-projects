7138882e538eb7f77c90aea048d1acab99080d4d
Zobam <upc4you@gmail.com>
2024-05-31 12:54:16 +0100

    feat: added the new logo and videos

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -36,7 +36,7 @@
     </main>
     <section id="video-container">
         <iframe width="560" height="315"
-            src="https://www.youtube.com/embed/z5_CtQ09snM?si=zdNNLpEtNF5_gjXM&autoplay=1&mute=1"
+            src="https://www.youtube.com/embed/Mi3QsF8AVL4?si=zdNNLpEtNF5_gjXM&autoplay=1&mute=1"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
